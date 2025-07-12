cd7f26e85b09b4a337f9a44fc6b0435fddd0678f
IwailerI <andrypirk@gmail.com>
2025-07-12 16:15:23 +0300

    added consistent 180 rotation

--- levels/player/player.gd
+++ levels/player/player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 enum CollisionAlgo {
-    BOUNCE,
-    SLIDE,
+	BOUNCE,
+	SLIDE,
 }
 
 @export var collision_algo := CollisionAlgo.BOUNCE
@@ -15,41 +15,43 @@ var direction: float = -PI * 0.5
 
 
 func _physics_process(delta: float) -> void:
-    var inp := Input.get_vector("movement_left", "movement_right", "movement_up", "movement_down")
-    var wanted_angle := 0.0
-    if inp.is_zero_approx():
-        wanted_angle = direction
-    else:
-        wanted_angle = inp.angle()
+	var inp := Input.get_vector("movement_left", "movement_right", "movement_up", "movement_down")
+	var wanted_angle := 0.0
+	if inp.is_zero_approx():
+		wanted_angle = direction
+	else:
+		wanted_angle = inp.angle()
 
-    direction = rotate_toward(direction, wanted_angle, rotation_speed * delta)
-    # prints(direction, wanted_angle)
+	if is_equal_approx(absf(angle_difference(direction, wanted_angle)), PI):
+		wanted_angle += 0.001 # always clockwise on 180 turn
 
-    $Polygon2D.rotation = direction + PI * 0.5
+	direction = rotate_toward(direction, wanted_angle, rotation_speed * delta)
 
-    var left := ground_speed * delta
+	$Polygon2D.rotation = direction + PI * 0.5
 
-    for i: int in 32:
-        var col := move_and_collide(Vector2.from_angle(direction) * left)
-        if col == null:
-            break
-        
-        var normal := col.get_normal()
-        global_position = col.get_position() + radius * normal
+	var left := ground_speed * delta
 
-        match collision_algo:
-            CollisionAlgo.BOUNCE:
-                direction = col.get_remainder().bounce(normal).angle()
-            CollisionAlgo.SLIDE:
-                var wall_angle := normal.angle() + PI * 0.5
-                var deflect := randf_range(0, PI * 0.2)
+	for i: int in 32:
+		var col := move_and_collide(Vector2.from_angle(direction) * left)
+		if col == null:
+			break
+		
+		var normal := col.get_normal()
+		global_position = col.get_position() + radius * normal
 
-                if absf(angle_difference(wall_angle, direction)) < absf(angle_difference(-wall_angle, direction)):
-                    direction = wall_angle + deflect
-                elif absf(angle_difference(wall_angle, direction)) > absf(angle_difference(-wall_angle, direction)):
-                    direction = -wall_angle - deflect
-                else:
-                    direction = wall_angle + deflect if randi() % 2 == 0 else -wall_angle - deflect
+		match collision_algo:
+			CollisionAlgo.BOUNCE:
+				direction = col.get_remainder().bounce(normal).angle()
+			CollisionAlgo.SLIDE:
+				var wall_angle := normal.angle() + PI * 0.5
+				var deflect := randf_range(0, PI * 0.2)
 
-        
-        left = col.get_remainder().length()
+				if absf(angle_difference(wall_angle, direction)) < absf(angle_difference(-wall_angle, direction)):
+					direction = wall_angle + deflect
+				elif absf(angle_difference(wall_angle, direction)) > absf(angle_difference(-wall_angle, direction)):
+					direction = -wall_angle - deflect
+				else:
+					direction = wall_angle + deflect if randi() % 2 == 0 else -wall_angle - deflect
+
+		
+		left = col.get_remainder().length()
