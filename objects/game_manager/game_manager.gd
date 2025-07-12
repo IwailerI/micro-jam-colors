8ba88148827f7d69fe19043bb37526e8d560afeb
HOPOLOK <vadim.truhanov@inbox.lv>
2025-07-12 18:22:33 +0300

    --fixup prev-hash

--- objects/game_manager/game_manager.gd
+++ objects/game_manager/game_manager.gd
@@ -10,13 +10,6 @@ var green: int = 0
 var blue: int = 0
 
 
-static func get_instance() -> GameManager:
-	var tree := Engine.get_main_loop() as SceneTree
-	var gms := tree.get_nodes_in_group("GameManager")
-	assert(len(gms) == 1, "Tried to get instance of the game manager while there is invalid amount of them.")
-	return gms[0]
-
-
 func add_colors(r: int, g: int, b: int) -> void:
 	red = min(red + r, MAX_COLOR_VALUE)
 	green = min(green + g, MAX_COLOR_VALUE)
