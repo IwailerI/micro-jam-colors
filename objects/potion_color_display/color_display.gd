77ad23fed4079fc80cb7155e652d65408dc8958a
IwailerI <andrypirk@gmail.com>
2025-07-13 14:40:18 +0300

    moved to rgb

--- objects/potion_color_display/color_display.gd
+++ objects/potion_color_display/color_display.gd
@@ -5,13 +5,13 @@ extends PanelContainer
 @export var color_good := Color.LIME
 @export var color_bad := Color.MAROON
 
-@onready var progress_c: ProgressBar = %ProgressC
-@onready var progress_m: ProgressBar = %ProgressM
-@onready var progress_y: ProgressBar = %ProgressY
+@onready var progress_r: ProgressBar = %ProgressR
+@onready var progress_g: ProgressBar = %ProgressG
+@onready var progress_b: ProgressBar = %ProgressB
 
-@onready var target_c: Panel = %TargetC
-@onready var target_m: Panel = %TargetM
-@onready var target_y: Panel = %TargetY
+@onready var target_r: Panel = %TargetR
+@onready var target_g: Panel = %TargetG
+@onready var target_b: Panel = %TargetB
 
 
 func _ready() -> void:
@@ -19,7 +19,7 @@ func _ready() -> void:
 	gm.added_colors.connect(func(c: int, m: int, y: int) -> void:
 		set_state(-Vector3i.ONE, Vector3i(c, m, y)))
 
-	set_state(Vector3i(gm.need_cyan, gm.need_magenta, gm.need_yellow), Vector3i.ZERO, 0)
+	set_state(Vector3i(gm.need_red, gm.need_green, gm.need_blue), Vector3i.ZERO, 0)
 
 
 func set_state(want: Vector3i, have: Vector3i, dur := NAN) -> void:
@@ -28,25 +28,25 @@ func set_state(want: Vector3i, have: Vector3i, dur := NAN) -> void:
 	if is_nan(dur):
 		dur = animation_duration
 	if want == -Vector3i.ONE:
-		want = Vector3i(gm.need_cyan, gm.need_magenta, gm.need_yellow)
+		want = Vector3i(gm.need_red, gm.need_green, gm.need_blue)
 	if have == -Vector3i.ONE:
-		have = Vector3i(gm.cyan, gm.magenta, gm.yellow)
+		have = Vector3i(gm.red, gm.green, gm.blue)
 
 	var t := create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE).set_parallel()
 
 	have = have.clamp(Vector3.ZERO, Vector3i.ONE * 4)
-	t.tween_property(progress_c, "value", have.x / 4.0, dur)
-	t.tween_property(progress_m, "value", have.y / 4.0, dur)
-	t.tween_property(progress_y, "value", have.z / 4.0, dur)
+	t.tween_property(progress_r, "value", have.x / 4.0, dur)
+	t.tween_property(progress_g, "value", have.y / 4.0, dur)
+	t.tween_property(progress_b, "value", have.z / 4.0, dur)
 
 	want = want.clamp(Vector3.ZERO, Vector3i.ONE * 4)
-	t.tween_property(target_c, "anchor_top", 1.0 - want.x / 4.0, dur)
-	t.tween_property(target_m, "anchor_top", 1.0 - want.y / 4.0, dur)
-	t.tween_property(target_y, "anchor_top", 1.0 - want.z / 4.0, dur)
+	t.tween_property(target_r, "anchor_top", 1.0 - want.x / 4.0, dur)
+	t.tween_property(target_g, "anchor_top", 1.0 - want.y / 4.0, dur)
+	t.tween_property(target_b, "anchor_top", 1.0 - want.z / 4.0, dur)
 
-	t.tween_property(target_c, "self_modulate", _get_target_col(have.x, want.x), dur)
-	t.tween_property(target_m, "self_modulate", _get_target_col(have.y, want.y), dur)
-	t.tween_property(target_y, "self_modulate", _get_target_col(have.z, want.z), dur)
+	t.tween_property(target_r, "self_modulate", _get_target_col(have.x, want.x), dur)
+	t.tween_property(target_g, "self_modulate", _get_target_col(have.y, want.y), dur)
+	t.tween_property(target_b, "self_modulate", _get_target_col(have.z, want.z), dur)
 
 
 func _get_target_col(have: int, want: int) -> Color:
