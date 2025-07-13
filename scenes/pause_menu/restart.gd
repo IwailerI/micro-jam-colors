30d952115d7a8390a06b545e678fb36e9cd8da86
HOPOLOK <vadim.truhanov@inbox.lv>
2025-07-13 14:13:31 +0300

    rm warning

--- scenes/pause_menu/restart.gd
+++ scenes/pause_menu/restart.gd
@@ -4,7 +4,7 @@ extends Button
 func _ready() -> void:
 	var next_id := Persistance.last_loaded_id + 1
 	GameManager.get_instance().game_was_over.connect(
-		func(has_won: bool, lost_message: String) -> void:
+		func(has_won: bool, _lost_message: String) -> void:
 			if not (next_id < Persistance.LEVEL_AMOUNT && has_won): grab_focus()
 	)
 
