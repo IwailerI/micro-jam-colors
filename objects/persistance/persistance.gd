9ddac50ab9ca9a621ca0b941c976a66db556ba9e
HOPOLOK <vadim.truhanov@inbox.lv>
2025-07-13 17:14:22 +0300

    fix lvl 10 in menu

--- objects/persistance/persistance.gd
+++ objects/persistance/persistance.gd
@@ -23,7 +23,7 @@ const LEVEL_NAME: Dictionary = {
 
 ## How many levels does the game has.
 ## Starts with 0 till the amount-1
-const LEVEL_AMOUNT: int = 10
+const LEVEL_AMOUNT: int = 11
 
 var completed_amount: int = 0
 var last_loaded_id: int = 0
